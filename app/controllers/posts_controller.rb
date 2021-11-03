a83dc05c5db6aee69dfd7f3762343e3247c90166
nadakichi <shogo.oilt@gmail.com>
2021-11-03 12:57:32 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
+  
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index
+    redirect_to action: :index  # 追記する
   end
+ 
 end
